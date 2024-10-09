ddbaa7a6920f35e802f561e7b30ae0cca9f3c29c
Stefan-011 <stefan.stanimirovic000@gmail.com>
2024-10-09 14:49:50 +0200

    Simulation buttons prep.

--- src/js/appFunctions.js
+++ src/js/appFunctions.js
@@ -6,51 +6,74 @@ import { RGB } from "./classes/rgb.js";
 const num_of_RGB = 3;
 const bit_size = 8;
 const NUM_OF_CHIPS = 16;
+
 export const SIGNAL_SIZE = 24;
 
 window.onload = () => {
-  const strip = new linkedlist();
-  let color_bits = [];
-  let red = new RGB();
-  red.setFullColor(255, 0, 0);
-  for (let i = 0; i < num_of_RGB; i++) {
-    switch (i) {
-      case 0:
-        toBinary(red.r, bit_size).then((bit_array) => {
-          bit_array.forEach((bit) => {
-            color_bits.push(bit);
-          });
-        });
-        break;
-      case 1:
-        toBinary(red.g, bit_size).then((bit_array) => {
-          bit_array.forEach((bit) => {
-            color_bits.push(bit);
-          });
-        });
-        break;
-      case 2:
-        toBinary(red.b, bit_size).then((bit_array) => {
-          bit_array.forEach((bit) => {
-            color_bits.push(bit);
-          });
-        });
-        break;
-    }
-  }
-  for (let i = 0; i < NUM_OF_CHIPS; i++) {
-    const new_chip = new chip(i * 100);
-    strip.addNode(new_chip);
-  }
-  let curr_chip = strip.getHead();
-  let iterator = 1;
-  let sender_inteval = setInterval(() => {
-    if (NUM_OF_CHIPS <= iterator) clearInterval(sender_inteval);
-    color_bits.forEach((bit) => {
-      curr_chip.receive_bit(bit);
-    });
-    iterator++;
-  }, 1000);
+  const RED_BUTTON = document.getElementById("red-sim");
+  const GREEN_BUTTON = document.getElementById("green-sim");
+  const BLUE_BUTTON = document.getElementById("blue-sim");
+  const RGB_BUTTON = document.getElementById("rgb-sim");
+  const BW_BUTTON = document.getElementById("bw-sim");
+
+  RED_BUTTON.addEventListener("click", () => {
+    alert("RED BTN PRESSED");
+  });
+  GREEN_BUTTON.addEventListener("click", () => {
+    alert("GREEN BTN PRESSED");
+  });
+  BLUE_BUTTON.addEventListener("click", () => {
+    alert("BLUE BTN PRESSED");
+  });
+  RGB_BUTTON.addEventListener("click", () => {
+    alert("RGB BTN PRESSED");
+  });
+  BW_BUTTON.addEventListener("click", () => {
+    alert("BW BTN PRESSED");
+  });
+
+  // const strip = new linkedlist();
+  // let color_bits = [];
+  // let red = new RGB();
+  // red.setFullColor(255, 0, 0);
+  // for (let i = 0; i < num_of_RGB; i++) {
+  //   switch (i) {
+  //     case 0:
+  //       toBinary(red.r, bit_size).then((bit_array) => {
+  //         bit_array.forEach((bit) => {
+  //           color_bits.push(bit);
+  //         });
+  //       });
+  //       break;
+  //     case 1:
+  //       toBinary(red.g, bit_size).then((bit_array) => {
+  //         bit_array.forEach((bit) => {
+  //           color_bits.push(bit);
+  //         });
+  //       });
+  //       break;
+  //     case 2:
+  //       toBinary(red.b, bit_size).then((bit_array) => {
+  //         bit_array.forEach((bit) => {
+  //           color_bits.push(bit);
+  //         });
+  //       });
+  //       break;
+  //   }
+  // }
+  // for (let i = 0; i < NUM_OF_CHIPS; i++) {
+  //   const new_chip = new chip(i * 100);
+  //   strip.addNode(new_chip);
+  // }
+  // let curr_chip = strip.getHead();
+  // let iterator = 1;
+  // let sender_inteval = setInterval(() => {
+  //   if (NUM_OF_CHIPS <= iterator) clearInterval(sender_inteval);
+  //   color_bits.forEach((bit) => {
+  //     curr_chip.receive_bit(bit);
+  //   });
+  //   iterator++;
+  // }, 1000);
 
   // let curr_chip2 = strip.getHead();
   // setTimeout(() => {
